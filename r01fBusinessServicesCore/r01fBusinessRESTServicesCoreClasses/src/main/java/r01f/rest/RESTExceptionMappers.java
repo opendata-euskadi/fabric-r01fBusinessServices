c42e3042e53dc123413959896e5a7ced7a74733c
IOLABARO <IOLABARO@B6001241.elkarlan.euskadi.eus>
2019-11-25 17:40:27 +0100

    minor changes

--- r01fBusinessServicesCore/r01fBusinessRESTServicesCoreClasses/src/main/java/r01f/rest/RESTExceptionMappers.java
+++ r01fBusinessServicesCore/r01fBusinessRESTServicesCoreClasses/src/main/java/r01f/rest/RESTExceptionMappers.java
@@ -5,7 +5,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import r01f.exceptions.Throwables;
 import r01f.model.persistence.PersistenceException;
 import r01f.model.persistence.PersistenceRequestedOperation;
@@ -21,14 +21,24 @@ import r01f.model.services.COREServiceException;
  * IMPORTANT!	Do NOT forget to include this types at the getClasses() method of {@link {AppCode}RESTApp} type
  * </pre>
  */
+@Slf4j
 public class RESTExceptionMappers {
 /////////////////////////////////////////////////////////////////////////////////////////
-//  
+//  RESTPersistenceExceptionMapper
 /////////////////////////////////////////////////////////////////////////////////////////
-	@RequiredArgsConstructor
+
 	public abstract static class RESTPersistenceExceptionMapper 
-	         		  implements ExceptionMapper<PersistenceException> {
+			extends RESTExceptionMapperBase<PersistenceException>		
+	     implements ExceptionMapper<PersistenceException> {
 		
+		public RESTPersistenceExceptionMapper(){
+			super();
+		}
+		
+		public RESTPersistenceExceptionMapper(final MediaType mediaType, final ExceptionToReponseEntity<PersistenceException> transformer){
+			super(mediaType,transformer);
+		 }
+	
 		@Override
 		public Response toResponse(final PersistenceException persistenceException) {			
 			Response outResponse = _handleThrowable(persistenceException);
@@ -36,112 +46,146 @@ public class RESTExceptionMappers {
 		}
 	}
 /////////////////////////////////////////////////////////////////////////////////////////
-//  
+//  RESTUncaughtExceptionMapper
 /////////////////////////////////////////////////////////////////////////////////////////
 	public static abstract class RESTUncaughtExceptionMapper 
-	         		  implements ExceptionMapper<Throwable> {
+			extends RESTExceptionMapperBase<Throwable>
+	       implements ExceptionMapper<Throwable> {
+		
+		public RESTUncaughtExceptionMapper(){
+			super();
+		}		
+		public RESTUncaughtExceptionMapper(final MediaType mediaType, final ExceptionToReponseEntity<Throwable> transformer){
+			super(mediaType,transformer);
+		 }
+		
 		@Override
 		public Response toResponse(final Throwable th) {
 			return _handleThrowable(th);
 		}
 	}
 /////////////////////////////////////////////////////////////////////////////////////////
-//  
+//  Exception to Response 
 /////////////////////////////////////////////////////////////////////////////////////////
-	/**
-	 * Maps an exception to an {@link HttpResponse}
-	 * The exception is built back at client side type: r01f.services.client.servicesproxy.rest.RESTResponseToCRUDResultMapperForModelObject
-	 * @param th
-	 * @return
-	 */
-	private static Response _handleThrowable(final Throwable th) {
-		// Print stack trace before any treatment (cause this could fail and mask the original Exception!!!!!) 
-		th.printStackTrace();
-		// serialize 
-		Response outResponse = null;
-		// Persistence exceptions
-		if (th instanceof COREServiceException) {
-			COREServiceException coreEx = (COREServiceException)th;
-			// server errors
-			if (coreEx.isServerError()) {			// Server Error
-				// force exception stack trace print
-				outResponse = Response.status(Status.INTERNAL_SERVER_ERROR)
-									  .header("x-r01-errorCode",COREServiceErrorTypes.SERVER_ERROR.encodeAsString())
-									  .header("x-r01-extErrorCode",coreEx.getExtendedCode())
-									  .header("x-r01-errorMessage",coreEx.getMessage())
-									  .header("x-r01-requestedOperation",coreEx.getCalledMethod().asString())
-									  .header("x-r01-errorType",coreEx.getClass().getName())
-									  .entity(Throwables.getStackTraceAsString(th))
-									  .type(MediaType.TEXT_HTML)
-									  .build();
-			} 
-			// client errors
-			else if (coreEx.isClientError()) {	
-				COREServiceErrorType coreErrorType = coreEx.getType();
-				
-				// record not found
-				if (coreErrorType.is(PersistenceServiceErrorTypes.ENTITY_NOT_FOUND)) {		
-					outResponse = Response.status(Status.NOT_FOUND)						
-										  .header("x-r01-errorCode",coreErrorType.encodeAsString())
-										  .header("x-r01-extErrorCode",coreEx.getExtendedCode())
-										  .header("x-r01-errorMessage",coreEx.getMessage())
-										  .header("x-r01-requestedOperation",coreEx.getCalledMethod().asString())
-										  .header("x-r01-errorType",coreEx.getClass().getName())
-										  .entity(Throwables.getStackTraceAsString(th))
-										  .type(MediaType.TEXT_HTML)
-										  .build();		
-				} 
-				// update requested but record existed OR the server version is different (optimistic locking)
-				else if (coreEx.getCalledMethod().isIn(PersistenceRequestedOperation.UPDATE.getCOREServiceMethod(),
-													   PersistenceRequestedOperation.CREATE.getCOREServiceMethod())
-				      && coreErrorType.isIn(PersistenceServiceErrorTypes.ENTITY_ALREADY_EXISTS,
-				    		  				PersistenceServiceErrorTypes.OPTIMISTIC_LOCKING_ERROR)) {
-					outResponse = Response.status(Status.CONFLICT)
-										  .header("x-r01-errorCode",coreErrorType.encodeAsString())
-										  .header("x-r01-extErrorCode",coreEx.getExtendedCode())
-										  .header("x-r01-errorMessage",coreEx.getMessage())
-										  .header("x-r01-requestedOperation",coreEx.getCalledMethod().asString())
-										  .header("x-r01-errorType",coreEx.getClass().getName())
-										  .entity(Throwables.getStackTraceAsString(th))
-										  .type(MediaType.TEXT_HTML)
-										  .build();						
-				}
-				// another bad client request
-				else {				
-					outResponse = Response.status(Status.BAD_REQUEST)
-										  .header("x-r01-errorCode",coreErrorType.encodeAsString())
-										  .header("x-r01-extErrorCode",coreEx.getExtendedCode())
-										  .header("x-r01-errorMessage",coreEx.getMessage())
-										  .header("x-r01-requestedOperation",coreEx.getCalledMethod().asString())
-										  .header("x-r01-errorType",coreEx.getClass().getName())
-										  .entity(Throwables.getStackTraceAsString(th))
-										  .type(MediaType.TEXT_HTML)
+	@FunctionalInterface
+	public static interface ExceptionToReponseEntity<T extends Throwable> {	
+		Object from( int errorCode, final T th); // Entity of rest requires 'Object'
+	}	
+/////////////////////////////////////////////////////////////////////////////////////////
+//  RESTExceptionMapperBase
+/////////////////////////////////////////////////////////////////////////////////////////	
+	 abstract static class RESTExceptionMapperBase<T extends Throwable>  {
+		 
+		 protected final MediaType _mediaType;
+		 protected final ExceptionToReponseEntity<T> _transformer;
+
+		 public RESTExceptionMapperBase(){// This has been the default behaviour for many years...
+			_mediaType = MediaType.TEXT_HTML_TYPE;
+			_transformer =  ( error, th )  -> { return  Throwables.getStackTraceAsString(th); };
+		}		 
+		 public RESTExceptionMapperBase(final MediaType mediaType, final ExceptionToReponseEntity<T> transformer){
+			_mediaType = mediaType;
+			_transformer = transformer;
+		} 		 
+		/**
+		 * Maps an exception to an {@link HttpResponse}
+		 * The exception is built back at client side type: r01f.services.client.servicesproxy.rest.RESTResponseToCRUDResultMapperForModelObject
+		 * @param th
+		 * @return
+		 */
+		@SuppressWarnings("unchecked")
+		protected  Response _handleThrowable(final Throwable th) {
+			// Print stack trace before any treatment (cause this could fail and mask the original Exception!!!!!) 
+			th.printStackTrace();
+			// serialize 
+			Response outResponse = null;
+			// Persistence exceptions
+			if (th instanceof COREServiceException) {				
+				COREServiceException coreEx = (COREServiceException)th;
+				// server errors
+				if (coreEx.isServerError()) {			
+					
+					// Server Error
+					// force exception stack trace print
+					log.error(" COREServiceException, server error");
+					outResponse = Response.status(Status.INTERNAL_SERVER_ERROR)
+											  .header("x-r01-errorCode",COREServiceErrorTypes.SERVER_ERROR.encodeAsString())
+											  .header("x-r01-extErrorCode",coreEx.getExtendedCode())
+											  .header("x-r01-errorMessage",coreEx.getMessage())
+											  .header("x-r01-requestedOperation",coreEx.getCalledMethod().asString())
+											  .header("x-r01-errorType",coreEx.getClass().getName())
+											  .entity(_transformer.from(COREServiceErrorTypes.SERVER_ERROR.getCode(), (T) th))
+											  .type(_mediaType)
 										  .build();
+				} 
+				// client errors
+				else if (coreEx.isClientError()) {	
+					COREServiceErrorType coreErrorType = coreEx.getType();
+				    log.error(" COREServiceException, client error");
+					// record not found
+					if (coreErrorType.is(PersistenceServiceErrorTypes.ENTITY_NOT_FOUND)) {		
+						outResponse = Response.status(Status.NOT_FOUND)						
+											  .header("x-r01-errorCode",coreErrorType.encodeAsString())
+											  .header("x-r01-extErrorCode",coreEx.getExtendedCode())
+											  .header("x-r01-errorMessage",coreEx.getMessage())
+											  .header("x-r01-requestedOperation",coreEx.getCalledMethod().asString())
+											  .header("x-r01-errorType",coreEx.getClass().getName())
+											  .entity(_transformer.from(Status.NOT_FOUND.getStatusCode(), (T) th))
+											  .type(_mediaType)
+											  .build();		
+					} 
+					// update requested but record existed OR the server version is different (optimistic locking)
+					else if (coreEx.getCalledMethod().isIn(PersistenceRequestedOperation.UPDATE.getCOREServiceMethod(),
+														   PersistenceRequestedOperation.CREATE.getCOREServiceMethod())
+					      && coreErrorType.isIn(PersistenceServiceErrorTypes.ENTITY_ALREADY_EXISTS,
+					    		  				PersistenceServiceErrorTypes.OPTIMISTIC_LOCKING_ERROR)) {
+						outResponse = Response.status(Status.CONFLICT)
+											  .header("x-r01-errorCode",coreErrorType.encodeAsString())
+											  .header("x-r01-extErrorCode",coreEx.getExtendedCode())
+											  .header("x-r01-errorMessage",coreEx.getMessage())
+											  .header("x-r01-requestedOperation",coreEx.getCalledMethod().asString())
+											  .header("x-r01-errorType",coreEx.getClass().getName())
+											  .entity(_transformer.from(Status.CONFLICT.getStatusCode(), (T) th))
+											  .type(_mediaType)
+											  .build();						
+					}
+					// another bad client request
+					else {				
+						outResponse = Response.status(Status.BAD_REQUEST)
+											  .header("x-r01-errorCode",coreErrorType.encodeAsString())
+											  .header("x-r01-extErrorCode",coreEx.getExtendedCode())
+											  .header("x-r01-errorMessage",coreEx.getMessage())
+											  .header("x-r01-requestedOperation",coreEx.getCalledMethod().asString())
+											  .header("x-r01-errorType",coreEx.getClass().getName())
+											  .entity(_transformer)
+											  .entity(_transformer.from(Status.BAD_REQUEST.getStatusCode(), (T) th))
+											  .build();
+					}
 				}
 			}
+			// Illegal argument exception
+			else if (th instanceof IllegalArgumentException) {
+				IllegalArgumentException illArgEx = (IllegalArgumentException)th;
+				outResponse = Response.status(Status.BAD_REQUEST)
+									  .header("x-r01-errorCode",COREServiceErrorTypes.BAD_CLIENT_REQUEST.encodeAsString())
+									  .header("x-r01-errorMessage",illArgEx.getMessage())
+									  .header("x-r01-errorType",illArgEx.getClass().getName())
+									  .entity(_transformer.from(Status.BAD_REQUEST.getStatusCode(), (T) th))
+									  .type(_mediaType)
+									  .build();
+			}
+			// any other exception type
+			else {
+				//th.printStackTrace();
+				outResponse = Response.status(Status.INTERNAL_SERVER_ERROR)
+									  .header("x-r01-errorCode",COREServiceErrorTypes.SERVER_ERROR.encodeAsString())
+									  .header("x-r01-errorMessage",th.getMessage())
+									  .header("x-r01-errorType",th.getClass().getName())
+									  .entity(_transformer.from(Status.INTERNAL_SERVER_ERROR.getStatusCode(), (T) th))
+									  .type(_mediaType)
+									  .build();
+			}
+			return outResponse;
 		}
-		// Illegal argument exception
-		else if (th instanceof IllegalArgumentException) {
-			IllegalArgumentException illArgEx = (IllegalArgumentException)th;
-			outResponse = Response.status(Status.BAD_REQUEST)
-								  .header("x-r01-errorCode",COREServiceErrorTypes.BAD_CLIENT_REQUEST.encodeAsString())
-								  .header("x-r01-errorMessage",illArgEx.getMessage())
-								  .header("x-r01-errorType",illArgEx.getClass().getName())
-								  .entity(Throwables.getStackTraceAsString(illArgEx))
-								  .type(MediaType.TEXT_HTML)
-								  .build();
-		}
-		// any other exception type
-		else {
-			//th.printStackTrace();
-			outResponse = Response.status(Status.INTERNAL_SERVER_ERROR)
-								  .header("x-r01-errorCode",COREServiceErrorTypes.SERVER_ERROR.encodeAsString())
-								  .header("x-r01-errorMessage",th.getMessage())
-								  .header("x-r01-errorType",th.getClass().getName())
-								  .entity(Throwables.getStackTraceAsString(th))
-								  .type(MediaType.TEXT_HTML)
-								  .build();
-		}
-		return outResponse;
 	}
 }
